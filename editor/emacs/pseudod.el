cd249c4d2e38cc8cb0ab03a514f56c2884b08b08
Alejandro Linarez Rangel <alinarezrangel@gmail.com>
2023-01-16 18:52:53 -0500

    emacs: Agrega más palabras clave al modo de Emacs.

--- editor/emacs/pseudod.el
+++ editor/emacs/pseudod.el
@@ -353,13 +353,14 @@ matched text.  This will happen before the body is evaluated."
     ("intenta" "atrapa" "finintenta")
     "adquirir" "variable" "variables" "instancia" "liberar"
     "fijar" "a"
+    "necesitas"
     "escribir" "leer"
     "devolver"
     "hereda" "extiende"
     "atributo" "atributos"
     "utilizar" "como"
     "son" "sean" "iguales" "diferentes" "y" "e"
-    "tanto" "algun" "o" "u"
+    "tanto" "algun" "o" "u" "no"
     "estatico" "abstracta" "ref" "desref")
   "List of all the keywords of PseudoD.
 
@@ -632,7 +633,7 @@ indentation (or a bigger indentation) then extra tabs are added."
 
 (defconst pseudod-font-lock-keywords
   ;; (insert (prin1-to-string (regexp-opt (flatten-list pseudod-keyword-list) 'words)))
-  '(("\\<\\(a\\(?:bstracta\\|dquirir\\|lgun\\|tr\\(?:apa\\|ibutos?\\)\\)\\|c\\(?:l\\(?:ase\\|onar\\)\\|o\\(?:mo\\|n\\)\\)\\|d\\(?:e\\(?:sref\\|volver\\)\\|iferentes\\)\\|e\\(?:s\\(?:cribir\\|tatico\\)\\|xtiende\\)\\|f\\(?:i\\(?:jar\\|n\\(?:cl\\(?:ase\\|onar\\)\\|funcion\\|i\\(?:\\(?:mplem\\|nt\\)enta\\)\\|llamar\\|m\\(?:etodo\\|ientras\\)\\|procedimiento\\|si\\)\\)\\|uncion\\)\\|hereda\\|i\\(?:guales\\|\\(?:mplement\\|n\\(?:stanci\\|tent\\)\\)a\\)\\|l\\(?:\\(?:ee\\|\\(?:iber\\|lam\\)a\\)r\\)\\|m\\(?:etodo\\|ientras\\)\\|procedimiento\\|ref\\|s\\(?:ean\\|i\\(?:no\\)?\\|on\\)\\|tanto\\|utilizar\\|variables?\\|[aeouy]\\)\\>" . font-lock-keyword-face)
+  '(("\\<\\(a\\(?:bstracta\\|dquirir\\|lgun\\|tr\\(?:apa\\|ibutos?\\)\\)\\|c\\(?:l\\(?:ase\\|onar\\)\\|o\\(?:mo\\|n\\)\\)\\|d\\(?:e\\(?:sref\\|volver\\)\\|iferentes\\)\\|e\\(?:s\\(?:cribir\\|tatico\\)\\|xtiende\\)\\|f\\(?:i\\(?:jar\\|n\\(?:cl\\(?:ase\\|onar\\)\\|funcion\\|i\\(?:\\(?:mplem\\|nt\\)enta\\)\\|llamar\\|m\\(?:etodo\\|ientras\\)\\|procedimiento\\|si\\)\\)\\|uncion\\)\\|hereda\\|i\\(?:guales\\|\\(?:mplement\\|n\\(?:stanci\\|tent\\)\\)a\\)\\|l\\(?:\\(?:ee\\|\\(?:iber\\|lam\\)a\\)r\\)\\|m\\(?:etodo\\|ientras\\)\\|n\\(?:ecesitas\\|o\\)\\|procedimiento\\|ref\\|s\\(?:ean\\|i\\(?:no\\)?\\|on\\)\\|tanto\\|utilizar\\|variables?\\|[aeouy]\\)\\>" . font-lock-keyword-face)
     ("\\([,;:()%]\\)" . font-lock-keyword-face))
   "Font locking for PseudoD keywords.")
 
